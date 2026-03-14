472ec759393f45822cc1e7114cccd15ea452b41a
David <Newsomedc@yahoo.com>
2026-03-14 00:27:17 -0500

    chore: force vercel rebuild with file change

--- src/main.jsx
+++ src/main.jsx
@@ -2,3 +2,4 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 ReactDOM.createRoot(document.getElementById("root")).render(<React.StrictMode><App /></React.StrictMode>);
+// Sat Mar 14 00:27:17 CDT 2026
